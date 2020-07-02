6cc4fb1b32af74ec3bcec6e9beea0e50b0801107
Matheus Oliveira <matheus_o._santos@outlook.com>
2020-07-02 14:25:57 -0300

    Criando operacao de instanciacao.

--- Projeto Estudo de caso com Java com Spring boot e JPA/CursoMC/src/main/java/com/matheusol/cursomc/CursoMcApplication.java
+++ Projeto Estudo de caso com Java com Spring boot e JPA/CursoMC/src/main/java/com/matheusol/cursomc/CursoMcApplication.java
@@ -1,12 +1,31 @@
 package com.matheusol.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.matheusol.cursomc.domain.Categoria;
+import com.matheusol.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursoMcApplication {
+public class CursoMcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoMcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(0, "Informática");
+		Categoria cat2 = new Categoria(0, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 }
