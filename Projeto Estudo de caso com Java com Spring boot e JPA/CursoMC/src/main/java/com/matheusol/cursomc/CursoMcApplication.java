5f6cc4bb6eb16a4dc0c0bf3c865697f0f1a7e759
Matheus Oliveira <matheus_o._santos@outlook.com>
2020-07-02 15:12:30 -0300

    Criacao da classe Produto e associacao muitos para muitos.

--- Projeto Estudo de caso com Java com Spring boot e JPA/CursoMC/src/main/java/com/matheusol/cursomc/CursoMcApplication.java
+++ Projeto Estudo de caso com Java com Spring boot e JPA/CursoMC/src/main/java/com/matheusol/cursomc/CursoMcApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.matheusol.cursomc.domain.Categoria;
+import com.matheusol.cursomc.domain.Produto;
 import com.matheusol.cursomc.repositories.CategoriaRepository;
+import com.matheusol.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursoMcApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoMcApplication.class, args);
@@ -26,6 +31,18 @@ public class CursoMcApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(0, "Informática");
 		Categoria cat2 = new Categoria(0, "Escritório");
 
+		Produto p1 = new Produto(0, "Computador", 2000.00);
+		Produto p2 = new Produto(0, "Impressora", 800.00);
+		Produto p3 = new Produto(0, "Mouse", 80.00);
+
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 }
