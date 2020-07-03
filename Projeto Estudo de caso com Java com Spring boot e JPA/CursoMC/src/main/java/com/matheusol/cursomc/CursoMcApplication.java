d587c16f80e8c3ef5c72e6a2e80c987543dbddee
Matheus Oliveira <matheus_o._santos@outlook.com>
2020-07-03 11:28:34 -0300

    Adicionando as entidades Pedido, EstadoPagamento e Pagamento

--- Projeto Estudo de caso com Java com Spring boot e JPA/CursoMC/src/main/java/com/matheusol/cursomc/CursoMcApplication.java
+++ Projeto Estudo de caso com Java com Spring boot e JPA/CursoMC/src/main/java/com/matheusol/cursomc/CursoMcApplication.java
@@ -1,5 +1,6 @@
 package com.matheusol.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.matheusol.cursomc.domain.Cidade;
 import com.matheusol.cursomc.domain.Cliente;
 import com.matheusol.cursomc.domain.Endereco;
 import com.matheusol.cursomc.domain.Estado;
+import com.matheusol.cursomc.domain.Pagamento;
+import com.matheusol.cursomc.domain.PagamentoComBoleto;
+import com.matheusol.cursomc.domain.PagamentoComCartao;
+import com.matheusol.cursomc.domain.Pedido;
 import com.matheusol.cursomc.domain.Produto;
+import com.matheusol.cursomc.domain.enums.EstadoPagamento;
 import com.matheusol.cursomc.domain.enums.TipoCliente;
 import com.matheusol.cursomc.repositories.CategoriaRepository;
 import com.matheusol.cursomc.repositories.CidadeRepository;
 import com.matheusol.cursomc.repositories.ClienteRepository;
 import com.matheusol.cursomc.repositories.EnderecoRepository;
 import com.matheusol.cursomc.repositories.EstadoRepository;
+import com.matheusol.cursomc.repositories.PagamentoRepository;
+import com.matheusol.cursomc.repositories.PedidoRepository;
 import com.matheusol.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -41,6 +49,12 @@ public class CursoMcApplication implements CommandLineRunner {
 
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+
+	@Autowired
+	private PedidoRepository pedidoRepository;
+
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoMcApplication.class, args);
@@ -80,11 +94,27 @@ public class CursoMcApplication implements CommandLineRunner {
 		Endereco e2 = new Endereco(0, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
 
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm");
+
+		Pedido ped1 = new Pedido(0, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(0, sdf.parse("10/10/2017 19:35"), cli1, e2);
+
+		Pagamento pagto1 = new PagamentoComCartao(0, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+
+		Pagamento pagto2 = new PagamentoComBoleto(0, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"),
+				null);
+		ped2.setPagamento(pagto2);
+
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 	}
 }
